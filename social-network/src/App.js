e0087712418c11c67d721193b8838e0715fc289e
Nikita <nikita.kutkovoy@gmail.com>
2022-01-10 02:58:19 +0200

    use browser routs, props, add new components

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -1,16 +1,41 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./Header/Header";
-import Navbar from "./Navbar/Navbar";
-import Profile from "./Profile/Profile";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Navbar from "./Components/Navbar/Navbar";
+import Profile from "./Components/Profile/Profile";
+import Dialogs from "./Components/Dialogs/Dialogs";
+import News from "./Components/News/News";
+import Music from "./Components/Music/Music";
+import Settings from "./Components/Settings/Settings";
 
-function App() {
+function App(props) {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar state={props.state.sidebar} />
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route
+              path="/header"
+              element={<Profile state={props.state.profilePage} />}
+            />
+            <Route
+              path="/profile"
+              element={<Profile state={props.state.profilePage} />}
+            />
+            <Route
+              path="/dialogs/*"
+              element={<Dialogs state={props.state.dialogsPage} />}
+            />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
